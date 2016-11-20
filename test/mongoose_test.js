7fbfe1ce5d49f5b949788c93b97e23f632749bd0
i143code <ranjanah143code@yahoo.com>
2016-11-20 12:12:51 -0500

    adding mongoose mocha promises

--- test/mongoose_test.js
+++ test/mongoose_test.js
@@ -1,13 +1,20 @@
 var mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise;
+
 var dburl = "mongodb://localhost/user_test";
 mongoose.connect(dburl);
+//before one executed once not evertime
+before((done) =>{
+  mongoose.connection
+          .once('open',() =>{
+            done();
+          })
+          .on('error',(error) => {
+            console.log('Warning',error);
+          });
+})
 
-mongoose.connection
-        .once('open',() => console.log("Good to go"))
-        .on('error',(error) => {
-          console.log('Warning',error);
-        });
 //add a hook to execute before any test methid
 beforeEach((done) => {
   //empty our database  and drop all the data..Pause mocha before running any test Takes time to run it.
